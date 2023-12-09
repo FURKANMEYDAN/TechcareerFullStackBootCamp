8e3771c92912c1c868a862c47f337ab0dd50efc3
FURKANMEYDAN <bfmeydan@gmail.com>
2023-12-09 23:35:27 +0300

    Bitirme İlk Aşama

---
+++ BitirmeÖdevi/İlkAşama/asp.net/bootcamp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using bootcamp.Models;
+
+namespace bootcamp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var kurs = new Bootcamp();
+        kurs.Id=1;
+        kurs.Title="fullstack";
+        kurs.Image="1.jpg";
+        kurs.Description="Blaghblah";
+        return View(kurs);
+    }
+}
